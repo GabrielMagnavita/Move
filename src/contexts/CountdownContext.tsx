f35a93c042a3218a302aa73b83a6255d422a89f9
Gabriel Magnavita <55340081+GabrielMagnavita@users.noreply.github.com>
2021-08-11 12:18:35 -0300

    Update CountdownContext.tsx

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps ) {
     const { startNewChallenge } = useContext(challengesContext);
     
-    const [time, setTime] = useState(0.1 * 60)
+    const [time, setTime] = useState(0.1 * 50)
     const [isActive, setIsActive] = useState(false)
     const [hasFinished, setHasFinished] = useState(false);
 
